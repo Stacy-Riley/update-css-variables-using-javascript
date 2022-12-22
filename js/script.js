ea2e55dec1b8a3bd0b0af0b1cf947b3dbdcbfcfd
stacy_r <stacyrileyca@gmail.com>
2022-12-22 14:14:02 -0500

    Created the functionality for js to access and control the variables created in css for the image element git status

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-const inputs = document.querySelectorAll('.controls input')
+//Variable created to connect to the input elements in HTML
+const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate(){
+function handleUpdate() {
     // console.log(this.value);
+
+    //Created to attach the px sizing to the input range number location
+    //or the empty string that results from the color input:
     const suffix = this.dataset.sizing || '';
-    console.log(suffix);
-    document.documentElement.style.setProperty(`--{this.name}`, this.value + suffix)
+    // console.log(suffix);
+    
+    //Connecting the style changes to the name, value, and suffix which is the px
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+
+//Event listeners to capture the changes as the user slides the input from left to right
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 
